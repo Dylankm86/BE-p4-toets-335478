90235e527eb7c45404c78f9f09dbc26b6178e99c
dylan kishoen <335478@student.mboutrecht.nl>
2023-06-13 13:43:59 +0200

    sixth commit

--- app/views/Homepage/lespakket/index.php
+++ app/views/Homepage/lespakket/index.php
@@ -1,11 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+    <!DOCTYPE html>
+    <html lang="nl">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Lespakket Overzicht</title>
+    </head>
+
+    <body>
+        <table>
+            <tr>
+                <th>Naam</th>
+                <th>Mobiel</th>
+                <th>Pakketnaam</th>
+                <th>Aantal rijlessen</th>
+                <th>Prijs</th>
+                <th>Startdatum</th>
+                <th>Datum rijbewijs gehaald</th>
+            </tr>
+            <?php foreach ($data['leerlingperlespakket'] as $row) : ?>
+                <tr>
+                    <td><?= $row['naam']; ?></td>
+                    <td><?= $row['mobiel']; ?></td>
+                    <td><?= $row['pakketnaam']; ?></td>
+                    <td><?= $row['aantallessen']; ?></td>
+                    <td><?= $row['prijs']; ?></td>
+                    <td><?= $row['startdatumrijlessen']; ?></td>
+                    <td><?= $row['datumrijbewijsgehaald']; ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    </body>
+
+    </html>
